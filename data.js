d4e0a93a7a88650b191a398693f00fdcee23cd1d
khushxxl <khushaal.choithramani@gmail.com>
2021-11-05 00:34:14 +0530

    project started

---
+++ data.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    id: 1,
+    title: "iPhone 13 Pro Max",
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-family-hero?wid=470&hei=556&fmt=png-alpha&.v=1631220221000",
+    desc: "Get ₹9000.00 - ₹46120.00 off when you trade in an iPhone 8 or newer*",
+    price: "1,19,900",
+  },
+  {
+    id: 2,
+    title: "14-inch MacBook Pro",
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp14-spacegray-select-202110?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1632788573000",
+    desc: "8-Core CPU 14-Core GPU 16GB Unified Memory 512GB SSD Storage",
+    price: "1,94,900",
+  },
+  {
+    id: 3,
+    title: "Apple Watch Series-7",
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MKUU3_VW_34FR+watch-45-alum-starlight-nc-7s_VW_34FR_WF_CO?wid=375&hei=356&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1632171096000%2C1631661833000",
+    desc: "Starlight Aluminium Case with Sport Band",
+    price: "41,900",
+  },
+  {
+    id: 4,
+    title: "AirPods (3rd generation)",
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1632861342000",
+    desc: " All-new contoured design with force sensor control and a shorter stem.",
+    price: "18,500",
+  },
+  {
+    id: 5,
+    title: "Buy HomePod mini",
+    image:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/homepod-mini-select-blue-202110_FV1?wid=934&hei=440&fmt=jpeg&qlt=95&.v=1633086025000",
+    desc: "iri is your intelligent assistant on HomePod mini. Just use your voice to check your calendar or the weather, set alarms and complete everyday tasks.",
+    price: "9900.00",
+  },
+];
+
+// {
+//     id: 2,
+//     title: "",
+//     image: "",
+//     desc: "",
+//     price: "",
+//   },
